25a63a633a8f56b6ecd1c7ba562aa08948f858fc
Michael Hohn <hohn@github.com>
2025-01-08 09:40:00 -0800

    breakage test

--- session/AddUser.java
+++ session/AddUser.java
@@ -2,6 +2,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+break!!!
+
 public class AddUser {
     public static Connection connect() {
         Connection conn = null;
